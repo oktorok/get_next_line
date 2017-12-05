5568db1203e11453999cf105853ddcfd72ef0b6e
Jaume GARCIA <jagarcia@e2r8p14.42.fr>
2017-12-05 12:07:00 +0100

    get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jagarcia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/30 12:49:31 by jagarcia          #+#    #+#             */
+/*   Updated: 2017/12/03 20:37:15 by jagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# define BUFF_SIZE 100000
+# define NEW(x) ft_strnew(x)
+# include <fcntl.h>
+
+int					get_next_line(const int fd, char **line);
+typedef struct		s_fd
+{
+	int				fd;
+	char			*buffer;
+	struct s_fd		*next;
+}					t_fd;
+
+#endif
